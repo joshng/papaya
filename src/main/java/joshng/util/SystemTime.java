c25f68afdc807ce011494909b1a978d3f01c9c5a
josh gruenberg <josh@joshng.com>
2015-01-21 13:59:30 -0800

    SystemTime.UTC_ZONE_ID

--- src/main/java/joshng/util/SystemTime.java
+++ src/main/java/joshng/util/SystemTime.java
@@ -1,12 +1,8 @@
 package joshng.util;
 
-import com.google.common.collect.Ordering;
-
 import java.time.ZoneId;
 
 public class SystemTime {
   public static final ZoneId PACIFIC_ZONE_ID = ZoneId.of("America/Los_Angeles");
-  @SuppressWarnings({"unchecked"})
-  public static final Ordering<Comparable> EARLIEST_TO_LATEST_ORDERING = Ordering.natural();
-  public static final Ordering<Comparable> LATEST_TO_EARLIEST_ORDERING = EARLIEST_TO_LATEST_ORDERING.reverse();
+  public static final ZoneId UTC_ZONE_ID = ZoneId.of("Z");
 }
